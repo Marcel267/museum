f73b1b5d00ff31360dad061f9e848a0525682193
Marcel Martinez Otano <m.otano@omsag.de>
2023-09-13 10:12:25 +0200

    Add prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image'
-
 export default function Home() {
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     JOJOJO
+    <main className="container mx-auto p-5">
+     jvjdfhjkfjkdklfhsl
     </main>
   )
 }
