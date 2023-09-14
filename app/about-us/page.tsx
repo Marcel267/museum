3895b438f5e7dbb1c98262c6deb2ee603ff860f8
Marcel Martinez Otano <m.otano@omsag.de>
2023-09-14 08:17:35 +0200

    Add shadcn ui

---
+++ app/about-us/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AboutUs() {
+  return (
+    <h1 className='text-3xl'>Über uns</h1>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import { ThemeProvider } from '@/components/theme-provider'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Navbar />
+          <main className="container mx-auto p-5">
+            {children}
+          </main>
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
