fdba9cee7e957bb91c14d02e58f38327d389d2d9
Marcel Martinez Otano <m.otano@omsag.de>
2023-09-14 10:52:22 +0200

    Add: nav links

---
+++ app/forum/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Forum() {
+    return (
+        <h1 className='text-3xl'>Forum</h1>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { ModeToggle } from "@/components/mode-toggle"
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
