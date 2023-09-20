590c3a59a114d189bee55b2e137d03716868f5b7
SebHsr <sebastian-hielscher@gmx.de>
2023-09-20 11:57:45 +0200

    Hallo

--- components/login-modal.tsx
+++ components/login-modal.tsx
@@ -6,17 +6,12 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-<<<<<<< HEAD
 import {InputEmail, InputUsername, InputUserPassword} from "@/components/input-field";
-=======
-import { InputEmail } from "@/components/input-field";
->>>>>>> 720b20c7e03231656b321d238d6d021cc08be0e5
 import RegisterTabs from "@/components/register-tabs";
 import { Button } from "./ui/button";
 
 export default function LoginModal() {
   return (
-<<<<<<< HEAD
       <Dialog>
         <DialogTrigger>Open</DialogTrigger>
         <DialogContent>
@@ -32,23 +27,6 @@ export default function LoginModal() {
           </DialogHeader>
         </DialogContent>
       </Dialog>
-=======
-    <Dialog>
-      <DialogTrigger>
-        <Button variant="outline">Open</Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>
-            <RegisterTabs />
-          </DialogTitle>
-          <DialogDescription>
-            <InputEmail />
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
->>>>>>> 720b20c7e03231656b321d238d6d021cc08be0e5
   )
 }
 
