e48313f0bc41d2d00cb4acd58aac08e80287e79a
Marcel Martinez Otano <m.otano@omsag.de>
2023-09-25 09:28:50 +0200

    WIP: Image on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button"
 import LoginModal from "@/components/login-modal";
+import Image from "next/image";
 
 export default function Home() {
 
   return (
     <>
-      <h1 className="text-3xl">Home</h1>
-      {/* <ModeToggle /> */}
+      <Image alt="Museumsgebäude" width={500}
+        height={500} src={"@/public/head_gebaeude_fassade_900_0.jpg"} />
     </>
   )
 }
