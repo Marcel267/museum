52e343d22301278c16b01a6073028a61a34be27e
Sebastian Hielscher <sebastian.hielscher@techem.de>
2023-09-28 11:31:47 +0200

    WIP: Datenschutz seite bearbeitet

--- app/data-privacy/page.tsx
+++ app/data-privacy/page.tsx
@@ -4,19 +4,21 @@ export default function AboutUs() {
   return (
       <>
         <div className='text-center max-w-2xl mx-auto'>
+          <br/>
+          <br/>
           <h1 className='text-3xl font-bold mb-4'>
             Datenschutzerklärung für das Moroseum - Museum der Dinosaurier & Steinzeit
           </h1>
-
+          <br/>
           <p className='mb-4'>
             Diese Datenschutzerklärung klärt Sie über die Art, den Umfang und Zweck der Verarbeitung von personenbezogenen Daten (nachfolgend kurz "Daten") im Rahmen der Nutzung unserer Website sowie der damit verbundenen Funktionen und Inhalte auf.
           </p>
-          <br/>
+
           <h2 className='text-2xl font-bold mb-2'>1. Verantwortliche Stelle</h2>
           <p>
             Verantwortlich für die Datenverarbeitung auf dieser Website ist:
             <br />
-            Moroseum - Museum der Melancholie und Kunst
+            Moroseum - Museum der Dinosaurier & Steinzeit
             <br />
             Musterstraße 1
             <br />
@@ -40,18 +42,36 @@ export default function AboutUs() {
           <p>
             Die Verarbeitung der Daten erfolgt zum Zweck der Bereitstellung der Inhalte und Funktionen unserer Website sowie zur Kommunikation mit Besuchern.
           </p>
-
-          {/* Weitere Abschnitte... */}
-
+          <br/>
+          <h2 className='text-2xl font-bold my-2'>4. Umgang mit Kontaktdaten</h2>
+          <p>
+            Wenn Sie uns per E-Mail kontaktieren, werden Ihre angegebenen Daten zwecks Bearbeitung der Anfrage und für den Fall von Anschlussfragen bei uns gespeichert. Diese Daten geben wir nicht ohne Ihre Einwilligung weiter.
+          </p>
+          <br/>
+          <h2 className='text-2xl font-bold my-2'>5. Cookies</h2>
+          <p>
+            Unsere Website verwendet Cookies. Dabei handelt es sich um kleine Textdateien, die auf Ihrem Endgerät gespeichert werden. Sie dienen dazu, unsere Website nutzerfreundlicher zu gestalten. Sie können die Speicherung von Cookies in den Einstellungen Ihres Browsers deaktivieren.
+          </p>
+          <br/>
+          <h2 className='text-2xl font-bold my-2'>6. Ihre Rechte</h2>
+          <ul className='list-disc ml-8'>
+            <li>Auskunft: Sie haben das Recht auf Auskunft über Ihre gespeicherten Daten.</li>
+            <li>Berichtigung: Sie können die Berichtigung unrichtiger Daten verlangen.</li>
+            <li>Löschung: Sie können die Löschung Ihrer Daten verlangen, sofern keine gesetzlichen Aufbewahrungspflichten bestehen.</li>
+            <li>Widerspruch: Sie können der Verarbeitung Ihrer Daten jederzeit widersprechen.</li>
+          </ul>
+          <br/>
           <h2 className='text-2xl font-bold my-2'>7. Änderung der Datenschutzerklärung</h2>
           <p>
             Diese Datenschutzerklärung kann angepasst werden. Die jeweils aktuelle Version ist auf unserer Website verfügbar.
           </p>
-
+          <br/>
           <h2 className='text-2xl font-bold my-2'>8. Kontakt</h2>
           <p>
-            Für Fragen zum Datenschutz kontaktieren Sie uns bitte unter info@moroseum-museum.de oder unter der oben angegebenen Adresse.
+            Für Fragen zum Datenschutz kontaktieren Sie uns bitte unter info@moroseum.de oder unter der oben angegebenen Adresse.
           </p>
+          <br/>
+          <br/>
         </div>
       </>
   );
