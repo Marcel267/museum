2904873ba63932f9d816884c6c4f022a34662dcb
Marcel Martinez Otano <m.otano@omsag.de>
2023-09-28 14:25:51 +0200

    WIP: Austellungen & Veranstaltungen

--- app/(service)/exhibition-and-events/page.tsx
+++ app/(service)/exhibition-and-events/page.tsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import buildingPic from "../../../public/gebaeude_wasser1.jpg";
 
 export default function ExhibitionAndEvents() {
-    return (
-        <h1 className='text-3xl'>Austellungen & Veranstaltungen</h1>
-    )
+  return (
+    <>
+      <h1 className="mb-5 text-3xl">Austellungen & Veranstaltungen</h1>
+      <div className="flex items-center justify-center">
+        <div className="grid grid-cols-4 gap-14 bg-red-400">
+          <Image
+            className="rounded-md"
+            alt="Museumsgebäude"
+            width={200}
+            // height={200}
+            src={buildingPic}
+          />
+          <Image
+            className="rounded-md"
+            alt="Museumsgebäude"
+            width={200}
+            // height={200}
+            src={buildingPic}
+          />
+          <Image
+            className="rounded-md"
+            alt="Museumsgebäude"
+            width={200}
+            // height={200}
+            src={buildingPic}
+          />
+          <Image
+            className="rounded-md"
+            alt="Museumsgebäude"
+            width={200}
+            // height={200}
+            src={buildingPic}
+          />
+          <Image
+            className="rounded-md"
+            alt="Museumsgebäude"
+            width={200}
+            // height={200}
+            src={buildingPic}
+          />
+          <Image
+            className="rounded-md"
+            alt="Museumsgebäude"
+            width={200}
+            // height={200}
+            src={buildingPic}
+          />
+          <Image
+            className="rounded-md"
+            alt="Museumsgebäude"
+            width={200}
+            // height={200}
+            src={buildingPic}
+          />
+          <Image
+            className="rounded-md"
+            alt="Museumsgebäude"
+            width={200}
+            // height={200}
+            src={buildingPic}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
