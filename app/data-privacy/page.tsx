6b35d7d9b3a7bd1305021b0bf125b83f8f3c8bf3
Sebastian Hielscher <sebastian.hielscher@techem.de>
2023-09-29 08:58:47 +0200

    Finished Datenschutz

--- app/data-privacy/page.tsx
+++ app/data-privacy/page.tsx
@@ -6,15 +6,15 @@ export default function AboutUs() {
         <div className='text-center max-w-2xl mx-auto'>
           <br/>
           <br/>
-          <h1 className='text-3xl font-bold mb-4'>
+          <h1 className='text-2xl font-bold mb-2'>
             Datenschutzerklärung für das Moroseum - Museum der Dinosaurier & Steinzeit
           </h1>
           <br/>
           <p className='mb-4'>
             Diese Datenschutzerklärung klärt Sie über die Art, den Umfang und Zweck der Verarbeitung von personenbezogenen Daten (nachfolgend kurz "Daten") im Rahmen der Nutzung unserer Website sowie der damit verbundenen Funktionen und Inhalte auf.
           </p>
-
-          <h2 className='text-2xl font-bold mb-2'>1. Verantwortliche Stelle</h2>
+          <br/>
+          <h2 className='text-xl font-bold mb-2'>1. Verantwortliche Stelle</h2>
           <p>
             Verantwortlich für die Datenverarbeitung auf dieser Website ist:
             <br />
@@ -29,7 +29,7 @@ export default function AboutUs() {
             E-Mail: info@moroseum.de
           </p>
           <br/>
-          <h2 className='text-2xl font-bold my-2'>2. Arten der verarbeiteten Daten</h2>
+          <h2 className='text-xl font-bold mb-2'>2. Arten der verarbeiteten Daten</h2>
           <ul className='list-disc ml-8'>
             <li>Kontaktdaten (z.B. Name, E-Mail-Adresse)</li>
             <li>Kommunikationsdaten (z.B. IP-Adresse, Geräteinformationen, Datum und Uhrzeit des Zugriffs)</li>
@@ -38,22 +38,22 @@ export default function AboutUs() {
             <li>Meta-/Kommunikationsdaten (z.B. Geräte-Informationen, IP-Adressen)</li>
           </ul>
           <br/>
-          <h2 className='text-2xl font-bold my-2'>3. Zweck der Verarbeitung</h2>
+          <h2 className='text-xl font-bold mb-2'>3. Zweck der Verarbeitung</h2>
           <p>
             Die Verarbeitung der Daten erfolgt zum Zweck der Bereitstellung der Inhalte und Funktionen unserer Website sowie zur Kommunikation mit Besuchern.
           </p>
           <br/>
-          <h2 className='text-2xl font-bold my-2'>4. Umgang mit Kontaktdaten</h2>
+          <h2 className='text-xl font-bold mb-2'>4. Umgang mit Kontaktdaten</h2>
           <p>
             Wenn Sie uns per E-Mail kontaktieren, werden Ihre angegebenen Daten zwecks Bearbeitung der Anfrage und für den Fall von Anschlussfragen bei uns gespeichert. Diese Daten geben wir nicht ohne Ihre Einwilligung weiter.
           </p>
           <br/>
-          <h2 className='text-2xl font-bold my-2'>5. Cookies</h2>
+          <h2 className='text-xl font-bold mb-2'>5. Cookies</h2>
           <p>
             Unsere Website verwendet Cookies. Dabei handelt es sich um kleine Textdateien, die auf Ihrem Endgerät gespeichert werden. Sie dienen dazu, unsere Website nutzerfreundlicher zu gestalten. Sie können die Speicherung von Cookies in den Einstellungen Ihres Browsers deaktivieren.
           </p>
           <br/>
-          <h2 className='text-2xl font-bold my-2'>6. Ihre Rechte</h2>
+          <h2 className='text-xl font-bold mb-2'>6. Ihre Rechte</h2>
           <ul className='list-disc ml-8'>
             <li>Auskunft: Sie haben das Recht auf Auskunft über Ihre gespeicherten Daten.</li>
             <li>Berichtigung: Sie können die Berichtigung unrichtiger Daten verlangen.</li>
@@ -61,12 +61,12 @@ export default function AboutUs() {
             <li>Widerspruch: Sie können der Verarbeitung Ihrer Daten jederzeit widersprechen.</li>
           </ul>
           <br/>
-          <h2 className='text-2xl font-bold my-2'>7. Änderung der Datenschutzerklärung</h2>
+          <h2 className='text-xl font-bold mb-2'>7. Änderung der Datenschutzerklärung</h2>
           <p>
             Diese Datenschutzerklärung kann angepasst werden. Die jeweils aktuelle Version ist auf unserer Website verfügbar.
           </p>
           <br/>
-          <h2 className='text-2xl font-bold my-2'>8. Kontakt</h2>
+          <h2 className='text-xl font-bold mb-2'>8. Kontakt</h2>
           <p>
             Für Fragen zum Datenschutz kontaktieren Sie uns bitte unter info@moroseum.de oder unter der oben angegebenen Adresse.
           </p>
