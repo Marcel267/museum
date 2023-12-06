ee2e703ace08bf482bf5433ddd98b50d26ba9712
kimae <kim.fath02@googlemail.com>
2023-12-06 14:15:24 +0100

    Über uns Text

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -2,47 +2,58 @@ import React from 'react'
 
 export default function AboutUs() {
   return (
-      <>
-      <h1 className='text-3xl'>Über uns</h1>
-          <div>
+      <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className='text-2xl'>Über uns</h1>
+          <div className="flex flex-col items-center justify-center h-screen">
               <br/>
-              <h2 className='text-xl font-bold mb-2'>
+              <br/>
+              <h2 className='text-l font-bold mb-2'>
                 Willkommen bei unserem Museum: Eine Reise in die Vergangenheit und die Welt des Wissens
               </h2>
-              <h2 className='text-sm mb-2'>
+              <div className="max-w-2xl text-center">
+              <h2 className='text-xs mb-2'>
                   Herzlich willkommen auf der Website unseres Museums! Wir freuen uns, Sie auf einer spannenden Entdeckungsreise durch die Welt der Geschichte, Kunst und Wissenschaft begrüßen zu dürfen. In unserem Über uns-Bereich möchten wir Ihnen einen Einblick in die Geschichte, die Vision und die Menschen hinter diesem einzigartigen Ort bieten.
               </h2>
+              </div>
               <br/>
-              <h2 className='text-Base font-bold mb-2'>
+              <h2 className='text-sm font-bold mb-2'>
                   Unsere Geschichte:
               </h2>
-              <h2 className='text-sm mb-2'>
+              <div className="max-w-2xl text-center">
+              <h2 className='text-xs mb-2'>
                   Unsere Museumsgeschichte reicht viele Jahrzehnte zurück, als eine leidenschaftliche Gruppe von Sammlern und Geschichtsbegeisterten den Grundstein für das schuf, was heute unser stolzes Museum ist. Seit unserer Gründung haben wir uns kontinuierlich weiterentwickelt und unser Angebot erweitert, um Ihnen ein breites Spektrum an Ausstellungen und Aktivitäten zu bieten.
               </h2>
+              </div>
               <br/>
-              <h2 className='text-Base font-bold mb-2'>
+              <h2 className='text-sm font-bold mb-2'>
                   Unsere Sammlungen:
               </h2>
-              <h2 className='text-sm mb-2'>
+              <div className="max-w-2xl text-center">
+              <h2 className='text-xs mb-2'>
                   Unsere Sammlungen umfassen eine beeindruckende Vielfalt von Artefakten, Kunstwerken, Exponaten und Dokumenten aus verschiedenen Epochen und Kulturen. Wir laden Sie ein, in die Vergangenheit einzutauchen, indem Sie unsere ständigen und wechselnden Ausstellungen erkunden. Sie werden erstaunliche Geschichten, bedeutende Kunstwerke und wissenschaftliche Durchbrüche erleben, die die Menschheit geprägt haben.
               </h2>
+              </div>
               <br/>
-              <h2 className='text-Base font-bold mb-2'>
+              <h2 className='text-sm font-bold mb-2'>
                   Unser Team:
               </h2>
-              <h2 className='text-sm mb-2'>
+              <div className="max-w-2xl text-center">
+              <h2 className='text-xs mb-2'>
                   Hinter den Kulissen arbeitet ein engagiertes Team von Fachleuten, um sicherzustellen, dass Ihr Besuch zu einem unvergesslichen Erlebnis wird. Unsere Mitarbeiter sind Experten auf ihren Gebieten und teilen Ihre Leidenschaft für Geschichte, Kunst und Wissenschaft. Sie stehen Ihnen gerne zur Verfügung, um Fragen zu beantworten und Ihr Museumserlebnis zu bereichern.
               </h2>
+              </div>
               <br/>
-              <h2 className='text-sm mb-2'>
+              <div className="max-w-2xl text-center">
+              <h2 className='text-xs mb-2'>
                   Wir laden Sie herzlich ein, unser Museum persönlich zu besuchen und die Faszination der Vergangenheit, der Kunst und der Wissenschaft hautnah zu erleben. Unsere Öffnungszeiten und Eintrittspreise finden Sie auf unserer Website.
               </h2>
+              </div>
               <br/>
               <h2 className='text-Base font-bold mb-2'>
                   Ihr Moroseum Team
               </h2>
 
           </div>
-      </>
+      </div>
   )
 }
