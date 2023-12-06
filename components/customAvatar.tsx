b430dff488c48340b3748a8ecb8c0fba4cb9b4a0
SebHsr <sebastian-hielscher@gmx.de>
2023-12-06 10:27:32 +0100

    WIP: Fixed Login Modal

--- components/customAvatar.tsx
+++ components/customAvatar.tsx
@@ -1,55 +1,56 @@
-'use client'
-
-import { Avatar as UIAvatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+"use client";
+import {
+  Avatar as UIAvatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar";
 import * as React from "react";
-import {usePathname} from "next/navigation";
+import { usePathname } from "next/navigation";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuTrigger
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import Link from "next/link";
-import {cn} from "@/lib/utils";
+import { cn } from "@/lib/utils";
 import LoginModal from "@/components/login-modal";
 
 export default function CustomAvatar() {
-
   const [pathname, setPathname] = React.useState(usePathname());
+  const [isLoginModalOpen, setLoginModalOpen] = React.useState(false);
+
+  const openLoginModal = () => {
+    setLoginModalOpen(true);
+  };
+
+  const closeLoginModal = () => {
+    setLoginModalOpen(false);
+  };
 
   return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <span className="flex cursor-pointer items-center text-sm font-medium text-muted-foreground">
-              <UIAvatar>
-                <AvatarImage src="" />
-                <AvatarFallback>+</AvatarFallback>
-              </UIAvatar>
-            </span>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuGroup>
-              <LoginModal/>
-                <DropdownMenuItem className="w-ful">
-                  Login / Registrieren
-                </DropdownMenuItem>
-              {/*<Link
-                  href={"/prices-and-hours"}
-                  className={cn(
-                      "flex items-center text-sm font-medium text-muted-foreground",
-                      pathname === "/prices-and-hours" && "text-foreground",
-                  )}
-                  onClick={() => setPathname("/prices-and-hours")}
-              >
-                <DropdownMenuItem className="w-full">
-                  Preise & Öffnungszeiten
-                </DropdownMenuItem>
-              </Link>*/}
-            </DropdownMenuGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
-
-
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <span
+          className="flex cursor-pointer items-center text-sm font-medium text-muted-foreground"
+          onClick={openLoginModal}
+        >
+          <UIAvatar>
+            <AvatarImage src="" />
+            <AvatarFallback>+</AvatarFallback>
+          </UIAvatar>
+        </span>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuGroup>
+          <LoginModal isOpen={isLoginModalOpen} onClose={closeLoginModal} />
+          <DropdownMenuItem
+            className="w-full"
+            onClick={openLoginModal}
+          ></DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
--- components/login-modal.tsx
+++ components/login-modal.tsx
@@ -1,26 +1,28 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import RegisterTabs from "@/components/register-tabs";
-import CustomAvatar from "@/components/customAvatar";
 
-export default function LoginModal() {
+interface LoginModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export default function LoginModal({ isOpen, onClose }: LoginModalProps) {
   return (
-    <Dialog>
+    <Dialog isOpen={isOpen} onClose={onClose}>
       <DialogTrigger>
-        {/*Trigger Einfügen*/}
+        <button onClick={onClose}>Close Dialog</button>
       </DialogTrigger>
       <DialogContent className="max-w-md">
         <DialogHeader>
           <DialogTitle>
             <RegisterTabs />
           </DialogTitle>
-          <DialogDescription></DialogDescription>
         </DialogHeader>
       </DialogContent>
     </Dialog>
